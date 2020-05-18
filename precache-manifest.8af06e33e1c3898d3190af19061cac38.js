91832d963e9ad969ea59ea80d06c9674c1dbf2a6
noahfoe <noahfoley6@gmail.com>
2020-05-17 19:21:47 -0500

    Updates

---
+++ precache-manifest.8af06e33e1c3898d3190af19061cac38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cb9a259b0820175f0476920a7f124a",
+    "url": "/Game-of-Life/index.html"
+  },
+  {
+    "revision": "4657441f4c75cc55500b",
+    "url": "/Game-of-Life/static/css/main.931dbbe8.chunk.css"
+  },
+  {
+    "revision": "57ed05c19faa0b0a0d6f",
+    "url": "/Game-of-Life/static/js/2.06c1501d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Game-of-Life/static/js/2.06c1501d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4657441f4c75cc55500b",
+    "url": "/Game-of-Life/static/js/main.efde0eaf.chunk.js"
+  },
+  {
+    "revision": "885c5e1c8a574ad6200e",
+    "url": "/Game-of-Life/static/js/runtime-main.6a5500ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Game-of-Life/static/media/logo.5d5d9eef.svg"
+  }
+]);
